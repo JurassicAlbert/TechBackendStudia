f4055c0909dc4a900ddd6bae34958fa1582d24e7
JurassicAlbert <albertjurasik@gmail.com>
2024-05-16 14:37:59 +0200

    implement DTOs

--- wsb-jpa1/src/main/java/com/capgemini/wsb/dto/AddressTO.java
+++ wsb-jpa1/src/main/java/com/capgemini/wsb/dto/AddressTO.java
@@ -1,6 +1,10 @@
 package com.capgemini.wsb.dto;
 
+import com.capgemini.wsb.persistence.entity.DoctorEntity;
+import com.capgemini.wsb.persistence.entity.PatientEntity;
+
 import java.io.Serializable;
+import java.util.List;
 
 public class AddressTO implements Serializable
 {
@@ -13,6 +17,10 @@ public class AddressTO implements Serializable
     private String addressLine2;
 
     private String postalCode;
+
+    private List<DoctorTO> doctors;
+
+    private List<PatientTO> patients;
 
     public Long getId() {
         return id;
@@ -52,5 +60,21 @@ public class AddressTO implements Serializable
 
     public void setPostalCode(String postalCode) {
         this.postalCode = postalCode;
+    }
+
+    public List<DoctorTO> getDoctors() {
+        return doctors;
+    }
+
+    public void setDoctors(List<DoctorTO> doctors) {
+        this.doctors = doctors;
+    }
+
+    public List<PatientTO> getPatients() {
+        return patients;
+    }
+
+    public void setPatients(List<PatientTO> patients) {
+        this.patients = patients;
     }
 }
