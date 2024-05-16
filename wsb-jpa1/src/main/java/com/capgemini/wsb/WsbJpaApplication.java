2f77724392985a0a225d04e2425247a6abc2da89
JurassicAlbert <albertjurasik@gmail.com>
2024-05-16 14:44:19 +0200

    fix issues with dao, enable repositories

--- wsb-jpa1/src/main/java/com/capgemini/wsb/WsbJpaApplication.java
+++ wsb-jpa1/src/main/java/com/capgemini/wsb/WsbJpaApplication.java
@@ -2,8 +2,10 @@ package com.capgemini.wsb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.capgemini.wsb.persistence.dao")
 public class WsbJpaApplication {
 
 	public static void main(String[] args) {
