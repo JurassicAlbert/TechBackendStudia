7da0daf048adfc74b90ab8128b41709a2f6c2304
JurassicAlbert <albertjurasik@gmail.com>
2024-05-17 14:22:40 +0200

    update Patient DTO

--- wsb-jpa1/src/main/java/com/capgemini/wsb/dto/PatientTO.java
+++ wsb-jpa1/src/main/java/com/capgemini/wsb/dto/PatientTO.java
@@ -7,7 +7,7 @@ import java.util.List;
 /**
  * The PatientTO class is a Data Transfer Object (DTO) that represents a patient.
  * It includes information such as the patient's ID, name, contact details, date of birth, insurance status,
- * associated addresses, and visits.
+ * associated visits, and a custom field of a type other than String.
  */
 public class PatientTO implements Serializable {
 
@@ -21,186 +21,97 @@ public class PatientTO implements Serializable {
     private String patientNumber;
     private LocalDate dateOfBirth;
     private boolean hasInsurance;
-    private List<AddressTO> addresses;
+    private int customField; // Custom field of type int
     private List<VisitTO> visits;
+    private List<AddressTO> addresses;
 
-    /**
-     * Gets the unique identifier of the patient.
-     *
-     * @return the ID of the patient
-     */
+    // Getters and setters for all fields
+
     public Long getId() {
         return id;
     }
 
-    /**
-     * Sets the unique identifier of the patient.
-     *
-     * @param id the new ID of the patient
-     */
     public void setId(Long id) {
         this.id = id;
     }
 
-    /**
-     * Gets the first name of the patient.
-     *
-     * @return the first name of the patient
-     */
     public String getFirstName() {
         return firstName;
     }
 
-    /**
-     * Sets the first name of the patient.
-     *
-     * @param firstName the new first name of the patient
-     */
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
 
-    /**
-     * Gets the last name of the patient.
-     *
-     * @return the last name of the patient
-     */
     public String getLastName() {
         return lastName;
     }
 
-    /**
-     * Sets the last name of the patient.
-     *
-     * @param lastName the new last name of the patient
-     */
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
 
-    /**
-     * Gets the telephone number of the patient.
-     *
-     * @return the telephone number of the patient
-     */
     public String getTelephoneNumber() {
         return telephoneNumber;
     }
 
-    /**
-     * Sets the telephone number of the patient.
-     *
-     * @param telephoneNumber the new telephone number of the patient
-     */
     public void setTelephoneNumber(String telephoneNumber) {
         this.telephoneNumber = telephoneNumber;
     }
 
-    /**
-     * Gets the email of the patient.
-     *
-     * @return the email of the patient
-     */
     public String getEmail() {
         return email;
     }
 
-    /**
-     * Sets the email of the patient.
-     *
-     * @param email the new email of the patient
-     */
     public void setEmail(String email) {
         this.email = email;
     }
 
-    /**
-     * Gets the patient number of the patient.
-     *
-     * @return the patient number of the patient
-     */
     public String getPatientNumber() {
         return patientNumber;
     }
 
-    /**
-     * Sets the patient number of the patient.
-     *
-     * @param patientNumber the new patient number of the patient
-     */
     public void setPatientNumber(String patientNumber) {
         this.patientNumber = patientNumber;
     }
 
-    /**
-     * Gets the date of birth of the patient.
-     *
-     * @return the date of birth of the patient
-     */
     public LocalDate getDateOfBirth() {
         return dateOfBirth;
     }
 
-    /**
-     * Sets the date of birth of the patient.
-     *
-     * @param dateOfBirth the new date of birth of the patient
-     */
     public void setDateOfBirth(LocalDate dateOfBirth) {
         this.dateOfBirth = dateOfBirth;
     }
 
-    /**
-     * Checks if the patient has insurance.
-     *
-     * @return true if the patient has insurance, false otherwise
-     */
     public boolean isHasInsurance() {
         return hasInsurance;
     }
 
-    /**
-     * Sets the insurance status of the patient.
-     *
-     * @param hasInsurance the new insurance status of the patient
-     */
     public void setHasInsurance(boolean hasInsurance) {
         this.hasInsurance = hasInsurance;
     }
 
-    /**
-     * Gets the list of addresses associated with the patient.
-     *
-     * @return the list of addresses
-     */
-    public List<AddressTO> getAddresses() {
-        return addresses;
+    public int getCustomField() {
+        return customField;
     }
 
-    /**
-     * Sets the list of addresses associated with the patient.
-     *
-     * @param addresses the new list of addresses
-     */
-    public void setAddresses(List<AddressTO> addresses) {
-        this.addresses = addresses;
+    public void setCustomField(int customField) {
+        this.customField = customField;
     }
 
-    /**
-     * Gets the list of visits associated with the patient.
-     *
-     * @return the list of visits
-     */
     public List<VisitTO> getVisits() {
         return visits;
     }
 
-    /**
-     * Sets the list of visits associated with the patient.
-     *
-     * @param visits the new list of visits
-     */
     public void setVisits(List<VisitTO> visits) {
         this.visits = visits;
+    }
+
+    public List<AddressTO> getAddresses() {
+        return addresses;
+    }
+
+    public void setAddresses(List<AddressTO> addresses) {
+        this.addresses = addresses;
     }
 }
