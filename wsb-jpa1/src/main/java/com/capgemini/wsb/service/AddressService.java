7fcf8bc1a625a8889d642c1727d2e4316d0cd15b
JurassicAlbert <albertjurasik@gmail.com>
2024-05-17 14:24:42 +0200

    update AddressService interface

--- wsb-jpa1/src/main/java/com/capgemini/wsb/service/AddressService.java
+++ wsb-jpa1/src/main/java/com/capgemini/wsb/service/AddressService.java
@@ -14,4 +14,27 @@ public interface AddressService {
      * @return the found address
      */
     AddressTO findById(Long id);
+
+    /**
+     * Adds a new address.
+     *
+     * @param addressTO the address data transfer object
+     * @return the added address
+     */
+    AddressTO addAddress(AddressTO addressTO);
+
+    /**
+     * Updates an existing address.
+     *
+     * @param addressTO the updated address data transfer object
+     * @return the updated address
+     */
+    AddressTO updateAddress(AddressTO addressTO);
+
+    /**
+     * Removes an address by its ID.
+     *
+     * @param id the ID of the address to remove
+     */
+    void removeAddress(Long id);
 }
