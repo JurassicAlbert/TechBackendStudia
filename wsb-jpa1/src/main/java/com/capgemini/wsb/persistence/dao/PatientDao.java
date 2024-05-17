537ec9948c8f514cb7be1111c7948619746695b5
JurassicAlbert <albertjurasik@gmail.com>
2024-05-17 14:21:30 +0200

    add required methods

--- wsb-jpa1/src/main/java/com/capgemini/wsb/persistence/dao/PatientDao.java
+++ wsb-jpa1/src/main/java/com/capgemini/wsb/persistence/dao/PatientDao.java
@@ -2,9 +2,43 @@ package com.capgemini.wsb.persistence.dao;
 
 import com.capgemini.wsb.persistence.entity.PatientEntity;
 
+import java.util.Collection;
+import java.util.List;
+
 /**
- * PatientDao is an interface that provides CRUD operations for PatientEntity base on Dao.
+ * PatientDao is an interface that provides CRUD operations for PatientEntity based on Dao.
  */
 public interface PatientDao extends Dao<PatientEntity, Long> {
 
+    /**
+     * Finds all patients who have more visits than the specified minimum number of visits.
+     *
+     * @param minNumberOfVisits the minimum number of visits
+     * @return a list of patients who have more visits than the specified minimum number
+     */
+    List<PatientEntity> findAllPatientsWithMoreVisitsThan(int minNumberOfVisits);
+
+    /**
+     * Finds all patients who are older than the specified age.
+     *
+     * @param age the age to compare
+     * @return a list of patients who are older than the specified age
+     */
+    List<PatientEntity> findPatientsOlderThan(int age);
+
+    /**
+     * Finds all patients who are younger than the specified age.
+     *
+     * @param age the age to compare
+     * @return a list of patients who are younger than the specified age
+     */
+    List<PatientEntity> findPatientsYoungerThan(int age);
+
+    /**
+     * Finds patients by their last name.
+     *
+     * @param lastName the last name to search for
+     * @return a collection of objects representing patients with the specified last name
+     */
+    Collection<Object> findByLastName(String lastName);
 }
--- wsb-jpa1/src/main/java/com/capgemini/wsb/persistence/dao/impl/PatientDaoImpl.java
+++ wsb-jpa1/src/main/java/com/capgemini/wsb/persistence/dao/impl/PatientDaoImpl.java
@@ -4,6 +4,10 @@ import com.capgemini.wsb.persistence.dao.PatientDao;
 import com.capgemini.wsb.persistence.entity.PatientEntity;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.TypedQuery;
+import java.util.Collection;
+import java.util.List;
+
 /**
  * PatientDaoImpl is an implementation of the PatientDao interface.
  * Provides CRUD operations for PatientEntity.
@@ -11,4 +15,59 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class PatientDaoImpl extends AbstractDao<PatientEntity, Long> implements PatientDao {
 
+    /**
+     * Finds all patients who have more visits than the specified minimum number of visits.
+     *
+     * @param minNumberOfVisits the minimum number of visits
+     * @return a list of patients who have more visits than the specified minimum number
+     */
+    @Override
+    public List<PatientEntity> findAllPatientsWithMoreVisitsThan(int minNumberOfVisits) {
+        TypedQuery<PatientEntity> query = entityManager.createQuery(
+                "SELECT p FROM PatientEntity p WHERE size(p.visits) > :minNumberOfVisits", PatientEntity.class);
+        query.setParameter("minNumberOfVisits", minNumberOfVisits);
+        return query.getResultList();
+    }
+
+    /**
+     * Finds all patients who are older than the specified age.
+     *
+     * @param age the age to compare
+     * @return a list of patients who are older than the specified age
+     */
+    @Override
+    public List<PatientEntity> findPatientsOlderThan(int age) {
+        TypedQuery<PatientEntity> query = entityManager.createQuery(
+                "SELECT p FROM PatientEntity p WHERE YEAR(CURRENT_DATE) - YEAR(p.dateOfBirth) > :age", PatientEntity.class);
+        query.setParameter("age", age);
+        return query.getResultList();
+    }
+
+    /**
+     * Finds all patients who are younger than the specified age.
+     *
+     * @param age the age to compare
+     * @return a list of patients who are younger than the specified age
+     */
+    @Override
+    public List<PatientEntity> findPatientsYoungerThan(int age) {
+        TypedQuery<PatientEntity> query = entityManager.createQuery(
+                "SELECT p FROM PatientEntity p WHERE YEAR(CURRENT_DATE) - YEAR(p.dateOfBirth) < :age", PatientEntity.class);
+        query.setParameter("age", age);
+        return query.getResultList();
+    }
+
+    /**
+     * Finds patients by their last name.
+     *
+     * @param lastName the last name to search for
+     * @return a collection of objects representing patients with the specified last name
+     */
+    @Override
+    public Collection<Object> findByLastName(String lastName) {
+        TypedQuery<Object> query = entityManager.createQuery(
+                "SELECT p FROM PatientEntity p WHERE p.lastName = :lastName", Object.class);
+        query.setParameter("lastName", lastName);
+        return query.getResultList();
+    }
 }
